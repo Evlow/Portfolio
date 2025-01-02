f144d47dde35c634522dc48d05fcf86623ac0c0a
Mathilde PEAUGER <mathilde.peauger@gmail.com>
2025-01-02 14:09:57 +0100

    Init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Bienvenue dans mon portfolio! PUTAIN CA ME SAOULE</h1>
+    </div>
+  );
+};
+
+export default App;
